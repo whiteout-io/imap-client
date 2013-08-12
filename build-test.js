b73b336e0cf13c2ec7537f9142c47a4ac069b933
Felix Hammerl <felix@whiteout.io>
2013-08-12 19:19:51 +0200

    Add browserify support for integration tests

---
+++ build-test.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var browserify = require('browserify'),
+    fs = require('fs'),
+    path = require('path');
+
+var b = browserify('./test/integration.js');
+b.require('net-chromeify', {
+    expose: 'net'
+});
+b.require('tls-chromeify', {
+    expose: 'tls'
+});
+b.require('brfs', {
+    expose: 'fs'
+});
+b.require('browserify-mime', {
+    expose: 'mime'
+});
+
+b.bundle(function(err, src) {
+    if (err) {
+        throw err;
+    }
+
+    var file = path.join(__dirname + '/test/integration-browserified.js');
+    fs.writeFileSync(file, src);
+
+    console.log('bundle written to: ' + file);
+});
