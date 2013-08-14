487fb1eb3349e9e8f07cec75aebde5a434991c0e
Tankred Hase <tankred@whiteout.io>
2013-08-14 11:11:50 +0200

    dir structure cleanup

--- build.js
+++ build.js
@@ -5,7 +5,7 @@ var browserify = require('browserify'),
     path = require('path');
 
 var b = browserify('./index.js');
-b.require('./iconv-dummy', {
+b.require('./src/iconv-dummy', {
     expose: 'iconv'
 });
 b.require('net-chromeify', {
@@ -19,11 +19,23 @@ b.require('browserify-mime', {
 });
 
 b.bundle(function(err, src) {
+    var dirName,
+        fileName,
+        file;
+
     if (err) {
         throw err;
     }
 
-    var file = path.join(__dirname + '/imap-client-browserified.js');
+    dirName = 'src-gen';
+    fileName = 'imap-client-browserified.js';
+
+    // create dir if not existant
+    try {
+        fs.mkdirSync(path.join(__dirname, dirName));
+    } catch (e) {}
+
+    file = path.join(__dirname, dirName, fileName);
     fs.writeFileSync(file, src);
 
     console.log('bundle written to: ' + file);
