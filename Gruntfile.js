3e8fa1c8be84f355664c85f84cd4781be9f57cb7
Tankred Hase <tankred@whiteout.io>
2013-08-17 15:45:39 +0200

    trying integration tests in travis

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,7 +14,7 @@ module.exports = function(grunt) {
                 options: {
                     reporter: 'spec'
                 },
-                src: ['test/unit-test.js']
+                src: ['test/*-test.js']
             }
         }
     });
